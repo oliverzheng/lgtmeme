7cc6d85507deecff4caca3e689f403b0260cb471
Oliver Zheng <gitcommits@oliverzheng.com>
2018-05-26 19:17:59 -0700

    Add mongodb connection, flow

---
+++ src/db.js
@@ -0,0 +1,20 @@
+// @flow
+
+import {MongoClient} from 'mongodb';
+import assert from 'assert';
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'lgtmeme';
+ 
+MongoClient.connect(url, {useNewUrlParser: true}, (err, client) => {
+  assert.equal(null, err);
+  console.log('Connected successfully to server');
+ 
+  const db = client.db(dbName);
+ 
+  client.close();
+});
+
+export default function(): number {
+  return 0;
+}
