12498fd0e6c857d8e65f70e7ee7b3775cebffad3
Oliver Zheng <gitcommits@oliverzheng.com>
2018-05-26 21:54:28 -0700

    Add prettier support

--- src/db.js
+++ src/db.js
@@ -1,17 +1,18 @@
 // @flow
+// @format
 
 import {MongoClient} from 'mongodb';
 import assert from 'assert';
 
 const url = 'mongodb://localhost:27017';
 const dbName = 'lgtmeme';
- 
+
 MongoClient.connect(url, {useNewUrlParser: true}, (err, client) => {
   assert.equal(null, err);
   console.log('Connected successfully to server');
- 
+
   const db = client.db(dbName);
- 
+
   client.close();
 });
 
