7d1b00dc5526743a8737b99401ee37786da4fe9f
Josh Carrier <joshjcarrier@gmail.com>
2018-06-02 15:04:25 -0700

    feat(graphql): Serves GraphQL and the GraphiQL editor (#13)
    Reduce overall API operation costs for client integration by providing GraphQL and its future pagination semantics instead of REST.

--- src/server/app.js
+++ src/server/app.js
@@ -2,11 +2,15 @@
 // @format
 
 import express, {type $Application, type $Response} from 'express';
+import bodyParser from 'body-parser';
+import {graphiqlExpress, graphqlExpress} from 'apollo-server-express';
+import {exampleQuery, schema} from './graphql';
 
 export function createApp(): $Application {
   const app = express();
 
-  app.get('/', (_, res: $Response) => res.send('Hello World!'));
+  app.use('/graphql', bodyParser.json(), graphqlExpress({schema: schema}));
+  app.use('/', graphiqlExpress({endpointURL: '/graphql', query: exampleQuery}));
 
   return app;
 }
