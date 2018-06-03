7a66b747ee7fa181cc5212afeabd25d7c2ec9752
Josh Carrier <joshjcarrier@gmail.com>
2018-06-02 19:25:26 -0700

    Pass the test

--- src/server/__tests__/graphqlTest.js
+++ src/server/__tests__/graphqlTest.js
@@ -5,6 +5,30 @@ import {graphql} from 'graphql';
 import {exampleQuery, schema} from '../graphql';
 
 test('validates fixture query', () => {
-  const expectedExecutionResult = `{"data": {"collection": {"id": "Collection/1", "memes": {"edges": [{"node": {"image": {"height": 300, "url": "https://user-images.githubusercontent.com/526858/40877498-a7df8ec2-6636-11e8-848d-88fd1af1e65f.jpg", "width": 200}, "macro": "ItS SheRAmiE"}}],"pageInfo": {"hasNextPage": false, "hasPreviousPage": false}}}}}`;
-  expect(graphql(schema, exampleQuery)).resolves.toBe(expectedExecutionResult);
+  const expectedExecutionResult = {
+    data: {
+      collection: {
+        id: 'Collection/1',
+        memes: {
+          edges: [
+            {
+              node: {
+                image: {
+                  height: 300,
+                  url:
+                    'https://user-images.githubusercontent.com/526858/40877498-a7df8ec2-6636-11e8-848d-88fd1af1e65f.jpg',
+                  width: 200,
+                },
+                macro: 'ItS SheRAmiE',
+              },
+            },
+          ],
+          pageInfo: {hasNextPage: false, hasPreviousPage: false},
+        },
+      },
+    },
+  };
+  expect(graphql(schema, exampleQuery)).resolves.toEqual(
+    expectedExecutionResult,
+  );
 });
