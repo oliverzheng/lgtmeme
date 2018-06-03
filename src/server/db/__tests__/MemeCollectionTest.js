0860e38fee14b208274ceba1bd6e28530fe90f58
Oliver Zheng <services+github@oliverzheng.com>
2018-06-02 23:48:25 -0700

    Add MemeCollection as a db model (#25)
    * Use mongoose and add flow-types
    * Add MemeCollection as a db model
    * Increase db test timeout

---
+++ src/server/db/__tests__/MemeCollectionTest.js
@@ -0,0 +1,43 @@
+// @flow
+// @format
+
+import invariant from 'invariant';
+import mockmongoose from '../mockmongoose';
+
+// This must be imported after mockgoose wrapper
+import {connect, disconnect} from '../';
+import MemeCollection from '../MemeCollection';
+
+// It's for db integration test - which has to pull in the in-memory mongodb
+// binary. We need to somehow cache the binary on CircleCI so this is faster in
+// the future.
+jest.setTimeout(30000);
+
+describe('MemeCollection', () => {
+  beforeAll(async () => {
+    await mockmongoose.prepareStorage();
+    await connect();
+  });
+
+  afterAll(async () => {
+    await disconnect();
+  });
+
+  test('should return no doc to start with', async () => {
+    // ID must be 24 characters
+    const obj = await MemeCollection.findById('123456789012345678901234');
+    expect(obj).toBeNull();
+  });
+
+  test('should return a saved doc with findById', async () => {
+    const collection = new MemeCollection();
+    collection.name = 'Ash Ketchum';
+    await collection.save();
+    expect(collection._id).not.toBeNull();
+
+    const obj: ?MemeCollection = await MemeCollection.findById(collection._id);
+    expect(obj).not.toBeNull();
+    invariant(obj, 'flow');
+    expect(obj.name).toBe('Ash Ketchum');
+  });
+});
