a8b7016fec48f40c7055037966a6e8c6aa58d55a
Josh Carrier <joshjcarrier@gmail.com>
2018-06-03 13:54:17 -0700

    tools(webpack): Replaces babel with webpack for Chrome extension
    This allows browsers to run compatible javascript and sets us up for additional frontend frameworks.

---
+++ src/chrome-extension/inline.js
@@ -0,0 +1,6 @@
+// @flow
+// @format
+declare var document: any;
+
+document.body.style.background =
+  'url(https://user-images.githubusercontent.com/526858/40891491-753b1a1c-673b-11e8-818f-1f6045779700.png)';
---
+++ src/chrome-extension/popup.js
@@ -0,0 +1,8 @@
+// @flow
+// @format
+declare var document: any;
+document.writeln(`<html>
+<body>
+  <h1>Shoop Da Woop!</h1>
+</body>
+</html>`);
