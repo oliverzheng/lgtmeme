17b2c1429bb5b5348cae8750e4005cd2052a8d4c
Oliver Zheng <gitcommits@oliverzheng.com>
2018-06-03 13:51:36 -0700

    Use `mongodb-memory-server` instead of `mockgoose`

--- src/server/db/__tests__/__mocks__/mockmongoose.js
+++ src/server/db/__tests__/__mocks__/mockmongoose.js
@@ -1,9 +1,37 @@
 // @flow
 // @format
 
-import mongoose from 'mongoose';
-
-import {Mockgoose} from 'mockgoose';
+import mongoose, {type MongooseConnection} from 'mongoose';
+import MongodbMemoryServer from 'mongodb-memory-server';
 
 mongoose.Promise = global.Promise;
-export default new Mockgoose(mongoose);
+
+export type Connection = {
+  mongooseConnection: MongooseConnection,
+  mongoServer: MongodbMemoryServer,
+};
+
+export async function connect(): Promise<Connection> {
+  const mongoServer = new MongodbMemoryServer();
+  const mongoUri = await mongoServer.getConnectionString();
+
+  return new Promise((resolve, reject) => {
+    const mongooseConnection = mongoose.createConnection();
+
+    // flow-typed doesn't have the 3-arg type definition :|
+    (mongooseConnection.openUri: any)(mongoUri, {useMongoClient: true}, err => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve({
+        mongooseConnection,
+        mongoServer,
+      });
+    });
+  });
+}
+
+export async function disconnect(connection: Connection): Promise<void> {
+  await connection.mongooseConnection.close();
+  await connection.mongoServer.stop();
+}
