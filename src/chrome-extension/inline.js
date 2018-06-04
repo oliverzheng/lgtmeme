8492f2468a1d79119e5e9df7d25729c5addaa348
Josh Carrier <joshjcarrier@gmail.com>
2018-06-03 20:46:28 -0700

    feat(chrome): Exact textarea value matching
    Watches specific textareas on GitHub on page load for change and replaces content when values match exactly.
    
    Areas of improvement:
    * attach/detatch event handling on element focus
    * fuzzy/substring match text contents
    * use macros list from server

--- src/chrome-extension/inline.js
+++ src/chrome-extension/inline.js
@@ -4,9 +4,42 @@
 declare var chrome: any;
 declare var document: Document;
 
+function keyEventHandler(event) {
+  const element = event.target;
+
+  switch (event.target.value.trim().toUpperCase()) {
+    case 'FUUU':
+      element.value =
+        '![fuuu](https://user-images.githubusercontent.com/526858/40897022-7226c9ca-676d-11e8-95e0-7d35ff2ad4ce.png)';
+      break;
+    case 'LGTM':
+      element.value =
+        '![lgtm](https://user-images.githubusercontent.com/526858/40896863-84ce611a-676c-11e8-8efe-d4383e187132.png)';
+      break;
+    case 'OHYOU':
+      element.value =
+        '![ohyou](https://user-images.githubusercontent.com/526858/40896996-44c01eb4-676d-11e8-9326-4f2fb0db2e98.jpeg)';
+      break;
+    case 'DEBUG':
+      chrome.runtime.sendMessage({text: element.value}, response => {
+        element.value = JSON.stringify(response.data);
+      });
+      break;
+    default:
+      break;
+  }
+}
+
 if (document.body) {
   document.body.style.background = // eslint-disable-line no-param-reassign
     'url(https://user-images.githubusercontent.com/526858/40891491-753b1a1c-673b-11e8-818f-1f6045779700.png)';
+
+  const els = document.querySelectorAll('textarea.comment-form-textarea');
+
+  els.forEach(el => {
+    const element = el;
+    element.onkeyup = keyEventHandler;
+  });
 }
 
 chrome.runtime.sendMessage({text: 'hello'}, response => {
