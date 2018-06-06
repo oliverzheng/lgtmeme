433aecf11243341117132d295c95c82ed0cc8870
Josh Carrier <joshjcarrier@gmail.com>
2018-06-06 00:14:08 -0700

    Inject database connection to make GraphQL layer configurable

--- src/server/graphql.js
+++ src/server/graphql.js
@@ -1,10 +1,11 @@
 // @flow
 // @format
 import fs from 'fs';
+import {GraphQLSchema} from 'graphql';
 import {connectionFromArray, fromGlobalId, globalIdField} from 'graphql-relay';
 import {makeExecutableSchema} from 'graphql-tools';
+import {type MongooseConnection} from 'mongoose';
 import path from 'path';
-import {connection} from 'mongoose';
 import {MemeDoc, getMemeModelForCollection} from './db/Meme';
 import {MemeCollectionDoc, getMemeCollection} from './db/MemeCollection';
 
@@ -15,52 +16,54 @@ const typeDefs = fs.readFileSync(
 
 type MemeFromCollection = {meme: MemeDoc, collection: MemeCollectionDoc};
 
-const resolvers = {
-  Collection: {
-    id: globalIdField(),
-    memes: async (collection: MemeCollectionDoc, args) => {
-      const Meme = getMemeModelForCollection(collection);
-      const memes = await Meme.find();
-      const memesWithSlug: Array<MemeFromCollection> = memes.map(meme => ({
-        meme,
-        collection,
-      }));
-      return connectionFromArray(memesWithSlug, args);
+export const createSchema = (connection: MongooseConnection): GraphQLSchema => {
+  const resolvers = {
+    Collection: {
+      id: globalIdField(),
+      memes: async (collection: MemeCollectionDoc, args) => {
+        const Meme = getMemeModelForCollection(collection);
+        const memes = await Meme.find();
+        const memesWithSlug: Array<MemeFromCollection> = memes.map(meme => ({
+          meme,
+          collection,
+        }));
+        return connectionFromArray(memesWithSlug, args);
+      },
     },
-  },
-  Image: {
-    height: ({meme}: MemeFromCollection) => meme.sourceImage.height,
-    url: ({meme, collection}: MemeFromCollection) =>
-      `http://localhost:3000/${collection.name}/${meme.sourceImage.fileID}`,
-    width: ({meme}: MemeFromCollection) => meme.sourceImage.width,
-  },
-  Meme: {
-    image: (memeFromCollection: MemeFromCollection) => memeFromCollection,
-    macro: ({meme}: MemeFromCollection) => meme.macro,
-  },
-  Node: {
-    __resolveType: () =>
-      // FIXME: this should be derivable from the object state.
-      'Collection',
-  },
-  Query: {
-    collection: async (_, {slug}) => {
-      const MemeCollection = getMemeCollection(connection);
-      const collection = await MemeCollection.findById(slug);
-      return collection;
+    Image: {
+      height: ({meme}: MemeFromCollection) => meme.sourceImage.height,
+      url: ({meme, collection}: MemeFromCollection) =>
+        `http://localhost:3000/${collection.name}/${meme.sourceImage.fileID}`,
+      width: ({meme}: MemeFromCollection) => meme.sourceImage.width,
     },
-    node: (_parent, {id}) => {
-      const {id: databaseId} = fromGlobalId(id);
-      const MemeCollection = getMemeCollection(connection);
-      return MemeCollection.findById(databaseId);
+    Meme: {
+      image: (memeFromCollection: MemeFromCollection) => memeFromCollection,
+      macro: ({meme}: MemeFromCollection) => meme.macro,
     },
-  },
-};
+    Node: {
+      __resolveType: () =>
+        // FIXME: this should be derivable from the object state.
+        'Collection',
+    },
+    Query: {
+      collection: async (_, {slug}) => {
+        const MemeCollection = getMemeCollection(connection);
+        const collection = await MemeCollection.findById(slug);
+        return collection;
+      },
+      node: (_parent, {id}) => {
+        const {id: databaseId} = fromGlobalId(id);
+        const MemeCollection = getMemeCollection(connection);
+        return MemeCollection.findById(databaseId);
+      },
+    },
+  };
 
-export const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
-});
+  return makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
+};
 
 export const exampleQuery = `
 query CollectionQuery {
