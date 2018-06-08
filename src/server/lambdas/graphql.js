b3178db8e449c3d05738ecddacab49eec063d91b
Josh Carrier <joshjcarrier@gmail.com>
2018-06-07 23:23:42 -0700

    Initialize lambda with S3 storage and update flow types.

--- src/server/lambdas/graphql.js
+++ src/server/lambdas/graphql.js
@@ -5,8 +5,17 @@ import {graphqlLambda} from 'apollo-server-lambda';
 import mongoose from 'mongoose';
 import {connect} from '../db';
 import {createSchema} from '../graphql';
+import S3Storage from '../storage/S3Storage';
+import S3StorageConfig from '../storage/S3StorageConfig';
+
+const s3Storage = new S3Storage(
+  S3StorageConfig.BUCKET_NAME,
+  S3StorageConfig.REGION,
+  S3StorageConfig.ACCESS_KEY_ID,
+  S3StorageConfig.SECRET_ACCESS_KEY,
+);
 
 connect();
-const schema = createSchema(mongoose.connection);
+const schema = createSchema(mongoose.connection, s3Storage);
 const handler = graphqlLambda({schema});
 export {handler};
