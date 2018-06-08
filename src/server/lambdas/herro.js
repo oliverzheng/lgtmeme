3506d82d039f22eb3f96b7713a5e7d481b8ffbfd
Oliver Zheng <services+github@oliverzheng.com>
2018-06-07 20:33:11 -0700

    Add netlify lambdas support (#52)

---
+++ src/server/lambdas/herro.js
@@ -0,0 +1,15 @@
+// @flow
+// @format
+
+import type {APIGatewayEvent, Context, ProxyCallback} from 'flow-aws-lambda';
+
+export function handler(
+  event: APIGatewayEvent,
+  context: Context,
+  callback: ProxyCallback,
+) {
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({msg: 'Herro, World!'}),
+  });
+}
