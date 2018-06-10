b4c9c9e1f6c8d8aa0e0c64c730fe31643a1f20ba
Oliver Zheng <gitcommits@oliverzheng.com>
2018-06-10 00:39:32 -0700

    Disconnect from mongo before finishing lambda

--- src/server/lambdas/graphql.js
+++ src/server/lambdas/graphql.js
@@ -2,29 +2,23 @@
 // @format
 
 import '@babel/polyfill';
-import {type MongooseConnection} from 'mongoose';
 import {graphqlLambda} from 'apollo-server-lambda';
 import type {APIGatewayEvent, Context, ProxyCallback} from 'flow-aws-lambda';
-import {connect} from '../db';
+import {connect, disconnect} from '../db';
 import {createSchema} from '../graphql';
 import s3Storage from '../storage/__mocks__/mockStorage'; // FIXME: Webpack load error of missing module require("domain")
 
-let connection: ?MongooseConnection = null;
-const connectPromise: Promise<MongooseConnection> = (async () => {
-  connection = await connect();
-  return connection;
-})();
-
 async function handleRequest(
-  conn: MongooseConnection,
   event: APIGatewayEvent,
   context: Context,
   callback: ProxyCallback,
 ) {
-  const schema = createSchema(conn, s3Storage);
+  const connection = await connect();
+  const schema = createSchema(connection, s3Storage);
 
   const requestOrigin = event.headers.origin;
   const callbackFilter = (error, output) => {
+    // This gets called before graphqlHandler() resolves
     if (requestOrigin && requestOrigin.startsWith('chrome-extension://')) {
       const {headers} = output;
       headers['Access-Control-Allow-Origin'] = requestOrigin;
@@ -32,7 +26,11 @@ async function handleRequest(
     callback(error, output);
   };
   const graphqlHandler = graphqlLambda({schema});
-  return graphqlHandler(event, context, callbackFilter);
+  await graphqlHandler(event, context, callbackFilter);
+
+  // Must disconnect, otherwise a lingering outbound connection makes lambda
+  // think we aren't done, and not return the request.
+  await disconnect();
 }
 
 export function handler(
@@ -40,9 +38,8 @@ export function handler(
   context: Context,
   callback: ProxyCallback,
 ) {
-  if (connection) {
-    handleRequest(connection, event, context, callback);
-  } else {
-    connectPromise.then(conn => handleRequest(conn, event, context, callback));
-  }
+  // handler cannot return a Promise - otherwise the callback data gets janky.
+  // That means handler cannot be an async function. Its content is pulled to
+  // handleRequest.
+  handleRequest(event, context, callback);
 }
