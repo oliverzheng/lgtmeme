c2a0dd04d8006e5055ed175e415d795388691838
Oliver Zheng <gitcommits@oliverzheng.com>
2018-06-09 23:22:32 -0700

    test

--- src/server/lambdas/herro.js
+++ src/server/lambdas/herro.js
@@ -2,6 +2,29 @@
 // @format
 
 import type {APIGatewayEvent, Context, ProxyCallback} from 'flow-aws-lambda';
+import {
+  MONGODB_HOST,
+  MONGODB_PORT,
+  MONGODB_USER,
+  MONGODB_PASS,
+  MONGODB_DB,
+  S3_BUCKET_NAME,
+  S3_REGION,
+  S3_ACCESS_KEY_ID,
+  S3_SECRET_ACCESS_KEY,
+} from '../env';
+
+const obj = {
+  MONGODB_HOST,
+  MONGODB_PORT,
+  MONGODB_USER,
+  MONGODB_PASS,
+  MONGODB_DB,
+  S3_BUCKET_NAME,
+  S3_REGION,
+  S3_ACCESS_KEY_ID,
+  S3_SECRET_ACCESS_KEY,
+};
 
 export function handler(
   event: APIGatewayEvent,
@@ -10,6 +33,8 @@ export function handler(
 ) {
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({msg: 'Herro, World!'}),
+    body: JSON.stringify({
+      msg: `Herro, World!\n${JSON.stringify(obj, null, ' ')}`,
+    }),
   });
 }
